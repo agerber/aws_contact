8d569848be9ce278ff73dd48222685c05024bc16
Adam Gerber <adam.gerber@yahoo.com>
2023-07-19 16:19:06 -0500

    add dir called EmailFunction

--- EmailFunction/app.py
+++ EmailFunction/app.py
@@ -1,10 +1,10 @@
+# all the following deps are referenced directly from the python sdk
 import json
 from typing import Dict
-from hello_world.message import Message
+from message import Message
 
 
 def lambda_handler(event, context):
-
 
     #headers we use to send back data to the caller
     headers: Dict[str, str] = {
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
             "statusCode": 400
         }
 
-    # info on how to use the context object https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
+
     # convert the message object back into a json
     json_object = {
 
@@ -32,8 +32,6 @@ def lambda_handler(event, context):
         "message-email": message.email
     }
 
-    # send out to SES
-
 
     # and return it
     return {
@@ -41,5 +39,4 @@ def lambda_handler(event, context):
         "headers": headers,
         "statusCode": 201
     }
-
 
